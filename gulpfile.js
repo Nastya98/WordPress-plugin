0b1097d897c14d8fba2a9383ab895658e686e9a0
Nastya98 <nastyamalinka@ukr.net>
2018-05-12 16:39:26 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('app/sass/**/*.sass')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('app/sass/**/*.sass', ['sass']);
+});
